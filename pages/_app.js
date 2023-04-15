be923582b9bf39e188caf2fca24d675885770290
Ulla Angell <ullaangell15@gmail.com>
2023-04-15 19:54:24 +0100

    setting the index and dleeting the styles for tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import 'no/styles/globals.css'
+import "no/styles/globals.css";
+import React from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
