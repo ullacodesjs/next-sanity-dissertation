352b090ed764199959af123782d53a9f1a49c2e1
Ulla Angell <ullaangell15@gmail.com>
2023-04-24 09:20:18 +0100

    x

--- sanity1682/schemas/index.js
+++ sanity1682/schemas/index.js
@@ -2,7 +2,5 @@ import blockContent from './blockContent'
 
 import product from './product'
 import user from './user'
-//import author from './author'
-//import author from './author'
 
 export const schemaTypes = [product, user, blockContent]
